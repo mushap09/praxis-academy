9891c05ecd4c57716c65a23a936a53148696f7c0
mushap09 <mustam_hp@yahoo.com>
2019-12-05 16:56:30 +0700

    commit

---
+++ novice/01-01/latihan/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main (String [] args ){
+        String belajar="java";
+        String menulis;
+        int tiga;
+        int a=3;
+        int penjumlahan=a+a;
+        System.out.print("hasil penjumlahan = "+penjumlahan);
+        int b=7;
+        int c=4;
+        int pengurangan=b-c;
+        System.out.print("hasil pengurangan = "+pengurangan);
+    }
+}
