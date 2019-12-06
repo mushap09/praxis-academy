d88bbd4cf7960745b60774db155e09678c5aca9d
mushap09 <mustam_hp@yahoo.com>
2019-12-06 17:59:42 +0700

    Latihan Hari Pertama

---
+++ novice/01-01/latihan/LatihanLagi.java
@@ -0,0 +1,11 @@
+public class LatihanLagi {
+    public static void main (String [] args){
+        String belajar="java";
+        String menghitung;
+        int a = 5;
+        int b = 2;
+        int penjumlahan = a+b;
+        System.out.println("hasil penjumlahan="+penjumlahan);
+
+    }
+}
