e86e1fe9f16d3b9e1e28ec726ae616ba0c03bd54
mushap09 <mustam_hp@yahoo.com>
2019-12-08 19:04:09 +0700

    Latihan Kasus aritmetika dan dicompile

---
+++ novice/01-01/Case/kasus/Aritmetika.java
@@ -0,0 +1,24 @@
+/**
+ * Aritmetika
+ */
+public class Aritmetika {
+
+    public static void main(String[] args) {
+
+        int penjumlahan = 100;
+        int pengurangan = 50;
+
+        int A = 20;
+        int B = 10;
+        int perkalian = A*B;
+        int pembagian = A/B;
+            
+        System.out.println(penjumlahan);
+        System.out.println(pengurangan);
+        System.out.println(perkalian);
+        System.out.println(pembagian);
+
+        
+        
+    }
+}
