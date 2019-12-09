1864cff7acb927844fc9766818ee1ab3442505ad
mushap09 <mustam_hp@yahoo.com>
2019-12-09 17:53:02 +0700

    Latihan OOP-01

---
+++ novice/01-02/latihan/Latihan-OOP-01/gradle/src/main/java/Latihan/OOP/BicycleDemo.java
@@ -0,0 +1,27 @@
+package main.java.Latihan.OOP;
+
+public class BicycleDemo {
+
+    public void BicycleDemoTest() {
+
+        // Create two different 
+        // Bicycle objects
+        Bicycle bike1 = new Bicycle();
+        Bicycle bike2 = new Bicycle();
+
+        // Invoke methods on 
+        // those objects
+        bike1.changeCadence(50);
+        bike1.speedUp(10);
+        bike1.changeGear(2);
+        bike1.printStates();
+
+        bike2.changeCadence(50);
+        bike2.speedUp(10);
+        bike2.changeGear(2);
+        bike2.changeCadence(40);
+        bike2.speedUp(10);
+        bike2.changeGear(3);
+        bike2.printStates();
+    }
+}
